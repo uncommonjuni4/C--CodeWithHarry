c23ecb65f747b8d2d3e0bf9e85ba4be73876eaba
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-04-11 19:18:12 +0500

    Add files via upload
    Work with break and continue statement

---
+++ C++/break-contniue statement.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace  std; 
+int main(){
+	
+	
+	
+	///  break amd continue statement in c++
+	
+	
+//	int i;
+//	for(i=0; i<=10 ; i++){
+//		cout<<i<<endl;
+//	
+//	
+//	 if(i == 6){
+//	 	
+//	 	
+//	 	cout<<"Welcome"<<endl;
+//	 break;
+//	 	
+//	 }
+//		
+//	
+//	}
+	
+         	int i;
+	for(i=0; i<=10 ; i++){
+		cout<<i<<endl;
+	
+	
+	 if(i == 6){
+	 	
+	 	
+	 	cout<<"Welcome"<<endl;
+	 continue;
+	 }
+		
+	
+	}
+
+	return 0;
+}
