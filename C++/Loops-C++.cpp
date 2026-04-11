835d51d657467759ac0d2c39eb169871118e3e5c
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-04-11 08:06:59 +0500

    C++ loops(While ,do while and for loop)
    C++ loops(While ,do while and for loop)

---
+++ C++/Loops-C++.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	//loops in c++
+	/*
+	there are three types of loops in C++
+	1: for Loop(the number or repeation is fixed in this loop)
+	2: while loop (condtion based)
+	3:do while loop(condtion based)
+  */
+  
+  //   for looop  
+  
+  
+  //  simple for loop example
+  
+//  for(int i = 1 ;i<=100;i++){
+//  	
+//cout<<"i = "<<i<<endl;
+//  }
+  ///infinite for looop expamole
+//  for(int i = 1 ;i>0;i++){
+//  	
+//cout<<"i = "<<i<<endl;
+//  }
+  
+////  example to print ALphabet using for loop
+//
+//  char word;
+//  for(word ='A' ;word<='Z'; word++){
+//  	
+//  	cout<<"Word :"<<word<<endl;
+//  }
+
+//  example to print ALphabet  revese order using for loop
+
+//  char word;
+//  for(word ='Z' ;word>='A'; word--){
+//  	
+//  	cout<<"Word :"<<word<<endl;
+//  }
+
+////  expamle to print table of 6 using for loop
+//  for(int i =1 ; i<=10 ; i++){
+//  	
+//  	cout<<"6 * "<<i<<"  ="<<i*6<<endl;
+//  }
+
+   
+   //for loop end here
+   
+   
+   //while loop
+//   
+//   int i = 2;
+//   while(i <=10){
+//   	cout<<i<<endl;
+//   	i++;
+//   };
+
+
+//  print alphbet using while loop  
+
+//    char word = 'A';
+//   while(word <='Z'){
+//   	cout<<word<<endl;
+//   	word++;
+//   };
+
+// print   reverse orderalphbet using while loop  
+//
+//    char word = 'Z';
+//   while(word >='A'){
+//   	cout<<word<<endl;
+//   	word++;
+//   };
+
+//print table of 6 using while loop 
+//
+//    char i = 	1;
+//   while(i <=10){
+//  	cout<<"6 * "<<i<<"  ="<<i*6<<endl;
+//   	i++;
+//   };
+
+
+  //while loop ended here
+  
+  //do while loop started here
+//  
+//  int i = 1 ;
+//  do{
+//  	
+//  	cout<<i<<endl;
+//  	i++;
+//  }while(i<=10);
+
+//   print alphabet using do while loop
+//  char word = 'A' ;
+//  do{
+//  	
+//  	cout<<word<<endl;
+//  	word++;
+//  }while(word<='Z');
+
+
+  // print  revesre order alphabet using do while loop
+//  char word = 'Z' ;
+//  do{
+//  	
+//  	cout<<word<<endl;
+//  	word--;
+//  }while(word>='A')
+
+
+//print table of sixes using do while loop;
+
+// int  i = 1;
+// 
+// do{
+// 		cout<<"6 * "<<i<<"  ="<<i*6<<endl;
+//   	i++;
+// }	while(i <=10);
+// 
+
+	return 0;
+}
