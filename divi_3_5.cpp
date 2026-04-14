6a73e4680ca6cab3c8021df99a71dc7c3f73151e
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-04-14 20:08:22 +0500

    check number  divsibkle by 3 and 5 using   for loop
    check number  divsibkle by 3 and 5 using   for loop

---
+++ divi_3_5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+      for(int i = 1 ; i<=100 ; i++){
+      	
+      	if(i  % 3== 0 && i % 5== 0){
+		  cout<<"Number "<<i<<"  is divible both by 3 and 5 "<<endl;
+		  }
+	  }
+    return 0;
+}
