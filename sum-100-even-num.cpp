14029dfd659c8b83a540d20578aeb44d144aad02
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-04-14 20:10:22 +0500

    SUM OF FIRST 100 EVEN NUMBERA USING FOR LOOP
    SUM OF FIRST 100 EVEN NUMBERA USING FOR LOOP

---
+++ sum-100-even-num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int sum =0;
+
+      for(int i = 1 ; i<=100 ; i++){
+      	
+      if(i%2==0){
+      	
+      	sum = sum + i;
+      	
+	  }
+	
+	  }
+	    cout<<"Sum of First 100 even number  = "<<sum;
+	  
+    return 0;
+}
