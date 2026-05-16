8c6e2c3ffbe8ced704ee32445ed51084fa3221c4
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 00:58:25 +0500

    For_while_dowhile_loops(10)
    For_while_dowhile_loops(10)

---
+++ c++ full course/For_while_dowhile_loops(10).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+
+    //    for loop ///  meean we want to do wokr same again and again by our own chice 
+
+
+    // it need , intailination , decalation , incremnnt  , condtion 
+    // for(int i = 1; i<=4; i++){
+    //     cout<<"Juni"<<endl;
+    // };
+
+
+    // while loop same as but diienfe is that  intiation and decalration can be out side , condtion
+
+    //  int a =1 ;  /// intailize 
+
+    //  while(a <5){    // condtion
+    //     cout<<"Juni"<<endl;
+    //     a++;/// incemnet 
+    //  };
+
+
+
+    // do while loopp  same  as while but its contion check at last 
+    // it will run at least one time beacise constion check at last
+ int a = 1 ;   /// intilze 
+   
+    do{
+        cout<<"Juni"<<endl;
+        a++;  //incemnet 
+    }while(a< 5); // condtion 
+    return  0;
+}
