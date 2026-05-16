f739c02f8943eb6400631937f7b92c23d92ed3a6
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 01:39:29 +0500

    Function_with_callByvalue_callByRefenece(15)
      // in call by value pass only value mean only sen dcopy of data , no the chnage occur in child refelt the parent func
    
    //    in call by refence we pass origani data mean every changing reflect in ervy plaxce

--- Function_with_callByvalue_callByRefenece(15).cpp
+++ Function_with_callByvalue_callByRefenece(15).cpp
@@ -2,7 +2,19 @@
 using namespace std ;
 #include<iomanip>
 
-  int funP(int a ){    // here we get passingf value    /// formal parameter  + call by valaye 
+//   int funP(int a ){    // here we get passingf value    /// formal parameter  + call by valaye 
+//    cout<<a;
+
+//    int b = 20;
+//    int temp = b;
+//     a  = temp; 
+//     cout<<a;
+
+   
+// }
+
+
+  int funP(int  &a ){    // here we get passingf value    /// formal parameter  + call by   refence (address) 
    cout<<a;
 
    int b = 20;
@@ -12,6 +24,7 @@ using namespace std ;
 
    
 }
+
 
 int  main(){
       
@@ -24,11 +37,30 @@ int  main(){
 
         // in call by value pass only value mean only sen dcopy of data , no the chnage occur in child refelt the parent func 
 
+    //       int a  =10;
+    
+    //   funP(a);     // function call  +  pass paramter from here    // actual paramter
+
+
+    //   cout<<a;  // a remain same 
+
+    //   \Function with  parameters   + call by value 
+
+    //        function para,eter mean we want to pass data form our parent funtction to child function 
+
+    //     type or parameter  and function must be same 
+
+    //    in call by refence we pass origani data mean every changing reflect in ervy plaxce
+
           int a  =10;
     
-      funP(a);     // function call  +  pass paramter from here    // actual paramter
+      funP(a);     // function call  +  pass paramter from here    // actual paramter  + call by refence 
 
 
-      cout<<a;  // a remain same 
+      cout<<a;  // a chnage 
+
+
+
+
           return  0;
 }
