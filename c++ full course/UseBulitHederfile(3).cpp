fd780b03bb5d7441b008383b83671c07361ea71e
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-16 23:24:57 +0500

    User define header file
    User define header file  mean we make our own header file

---
+++ c++ full course/UseBulitHederfile(3).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+#include<header.h>  /// her i call herder file 
+
+int  main(){
+   ////  userdefine header file mean we make  our own hearder file 
+
+
+    //   for exoamle i make header fil with name of "header.j"
+
+
+    return  0;
+}
