8ce901c4153303c3ffacf6d548e10bcb6ea8556b
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 00:12:38 +0500

    ////Assignments operatore
        //   assignmnest in c++ are use to assignm value to varibale with help of  arithmetic OPERATORS.some commen assigmnt oeratore __are_same
        //    1--- Equal (=)
        //    2----Equal to (==)
        //    3 ===greater (>)
        //   4 ==greater 0r eqaual (>=)
        //  5===less (<)
        //   6 ==less 0r eqaual (<=)

---
+++ c++ full course/Assignmnentoperators(7).cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+
+      ////Assignments operatore 
+    
+    //   assignmnest in c++ are use to assignm value to varibale with help of  arithmetic OPERATORS.some commen assigmnt oeratore __are_same
+    //    1--- Equal (=)
+    //    2----Equal to (==)
+    //    3 ===greater (>)
+    //   4 ==greater 0r eqaual (>=)
+    //  5===less (<)
+    //   6 ==less 0r eqaual (<=)
+
+    //   1--- Equal (=)
+
+    int  a =1;
+    int b ;
+
+    b= a;
+
+    cout<<"A == "<<a<<endl;
+    cout<<"B == "<<b<<endl;
+
+    //  same as next use but remianin use in contions 
+    
+    return  0;
+}
