5c41e67d8369af2a5409cde090260144ee0c0360
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 00:38:25 +0500

    Switch statement
    Switch statement  same to if else if

---
+++ c++ full course/Switch(9).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+
+  int a  = 3;
+  
+
+// --- switch is smilair to  if else but only stututre different 
+
+  switch (a)
+  {
+  case  3:
+    /* code */
+    cout<<"welcome Juni";
+    break;
+  
+  default:
+    break;
+  }
+    return  0;
+}
