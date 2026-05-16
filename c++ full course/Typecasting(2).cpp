ef31531ed159dd4b4e49a07e7e9526a5eac69d07
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-16 23:15:16 +0500

    Here work on c++  from baby steps
    Here work on c++  from baby steps
    1)   rule to decalre varibale
     2 )  typscasting   and its types

---
+++ c++ full course/Typecasting(2).cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+
+      //Two type of typecasting 
+      // 1----  implict typecasting    (autpmatically done   by  complier)
+      //2-----  Explicit typecasting  (manual   by progaramer )
+
+    //   -------------------------mplict typecasting  
+
+
+//     int   a  = 10;
+//        cout<<"Before Typecasting   a = "<<a<<endl;
+//       float   b = 10.11;
+
+//       bool   c = true;
+//       cout<<"value of c   = "<<c<<endl;
+//        a = b;
+//          c = a ;
+   
+//       cout<<"After Typecasting   b = "<<b<<endl;
+//   cout<<"value of c   = "<<c<<endl;  //  not type cast 
+
+
+  ////////explict type asting 
+
+
+
+    //  int  a = 12.11;
+
+    //    cout<<"Before Typecasting   a = "<<a<<endl;
+
+    //  bool b  = true ;
+
+    //  int Btypecast = int(a);
+
+
+    //  float Ctypecast  = float(a);
+
+
+
+
+
+    //  cout<<"After Typecasting   b = "<<Btypecast<<endl;
+    //  cout<<"After Typecasting   b = "<<Ctypecast<<endl;
+
+
+
+
+    return  0;
+}
---
+++ c++ full course/VariableRule(1).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+
+  //  Rule 1 variable name Not begen with Symbol or character 
+
+//    int # = 0;
+
+    //  int  ! = 0;
+     
+//      int @, %,^,&,*;
+
+//    cout<<@<<%<<^<<&<<*;
+
+    //  Rule 2 variable name Not begen with   numbers  
+    //   int 1 ; 
+    //   cout<<1;
+
+
+    // Rule 3 vairble   name    no be    keyword
+
+    // int false ;
+    // cout<<false;
+    return  0;
+}
