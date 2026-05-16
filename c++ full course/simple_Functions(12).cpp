603abc00532661d1a59b277622b0df1d075e6673
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 01:15:43 +0500

    Functions :
    Functions :
                    //    function mena wee write the code in one place and ue it again and again

---
+++ c++ full course/simple_Functions(12).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+#include<iomanip>
+
+
+void  fun(){     ///  function decalartion 
+    cout<<"Juni";  
+}
+
+int  main(){
+      
+    // --------Functions :
+                //    function mena wee write the code in one place and ue it again and again 
+
+
+          fun();   /// function calling 
+                
+
+
+    return  0;
+}
