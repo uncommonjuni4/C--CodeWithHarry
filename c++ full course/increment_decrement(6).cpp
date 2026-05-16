1bb962a01fbe72572f9a5e5282469ebb81eeacf2
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 00:05:22 +0500

    Increment or drement
    // incermnet operatore are use to   to incrase the value of varibale by 1
        //   Two types
        // 1----postincrment
        // 2---preincerment
    
        // 1-----postIncrement   incrase the value of 1 after the variable use one time at any place
    
     //2-- preuncrement mean first incrase then work
    
        // // 1-----postdecrement   decrase the value of 1 after the variable use one time at any place
         //2-- preuncrement mean first decrase then work

---
+++ c++ full course/increment_decrement(6).cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+
+    //// incermnet operatore are use to   to incrase the value of varibale by 1 
+    //   Two types
+    // 1----postincrment 
+    // 2---preincerment 
+
+
+
+    // 1-----postIncrement   incrase the value of 1 after the variable use one time at any place
+
+    // int a  = 1;
+    // cout<<"value of  a  before icerment  ="<<a<<endl;   ///  no incrase 
+    // a++;
+   
+    // cout<<"value of  a  after icerment  ="<<a<<endl;   /// incease 
+
+    //2-- preuncrement mean first incrase then work
+     
+    
+    // int a  = 1;
+    // ++a;
+    // cout<<"value of  a  before icerment  ="<<a<<endl;   ///  no incrase 
+    
+   
+    // cout<<"value of  a  after  icerment  ="<<a<<endl;   /// incease 
+
+
+    
+
+    // // 1-----postdecrement   decrase the value of 1 after the variable use one time at any place
+
+    // int a  = 1;
+    // cout<<"value of  a  before decerment  ="<<a<<endl;   ///  no decrase 
+    // a--;
+   
+    // cout<<"value of  a  after decerment  ="<<a<<endl;   /// decrease 
+
+
+     //2-- preuncrement mean first decrase then work
+     
+    
+    // int a  = 1;
+    // --a;
+    // cout<<"value of  a  before decerment  ="<<a<<endl;   ///  decrase 
+    
+   
+    // cout<<"value of  a  after decerment  ="<<a<<endl;   /// decrease 
+
+    return  0;
+}
