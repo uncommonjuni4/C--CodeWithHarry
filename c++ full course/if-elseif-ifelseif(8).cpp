00c019ec3985793cc9e256fd250ea181f8704717
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-17 00:23:16 +0500

    // 2------selection structure: //                            mean code flow on the base of condtions (if , else if , if else if ,
    // 2------selection structure:
    //                            mean code flow on the base of condtions (if , else if , if else if , switch)

---
+++ c++ full course/if-elseif-ifelseif(8).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+
+                            
+// 2------selection structure:
+//                            mean code flow on the base of condtions (if , else if , if else if , switch)
+
+
+   /// if else 
+
+
+   int a = 1 ;
+   int b = 2;
+
+//    if(a < b){
+//    cout<<" a greater then b ";
+//    }
+     
+
+      ///  else if  ager first contion  false then 2nd 
+      
+//       if(a>b){
+// cout<<" a greater then b ";
+//       }else if(a<b){
+//      cout<<" a less  then b ";
+//       }
+    
+
+         ///if   else if  ager first contion  false then 2nd flase yhen last 
+//    if(a>b){
+// cout<<" a greater then b ";
+//       }else if(a=b){
+//      cout<<" a less  then b ";
+//       }else{
+//         cout<<"nothing ";
+//       }
+         
+    return  0;
+}
