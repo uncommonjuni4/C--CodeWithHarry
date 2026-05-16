103f0d319ed569d1d1b5a455bd1eef76c6bc77aa
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-16 23:36:25 +0500

    Arithmethic Operators
    Arithmethic Operators   which are use to perform  mathtimaticall operations

---
+++ c++ full course/ArithmeticOpertator(4).cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std ;
+
+
+int  main(){
+   
+     ////Arithmetic operator are use to perform Mathmetical Operator 
+       ///there are different type of assigmnet operator are but some most common use operators are :
+       /* 
+        1 ---- Addtion (add Number) - "+"
+        2-----Subtraction (subtract numbers)  -"-"
+        3-----Mutliplication(multi  numbers)   -"*"
+        4-----division (divide  numbers )     -"/"
+        5-----Moduls  (fiind  reminder after diviions)  -"%"   */
+
+        // 1--- addtion 
+
+        cout<<12 + 4 <<endl;
+
+         // 2--- subtractioin 
+
+        cout<<12 - 4 <<endl;
+
+          // 3--- Multiplion 
+
+        cout<<12 * 4 <<endl;
+
+          // 4--- Divsion 
+
+        cout<<12 / 4 <<endl;
+
+          // 5--- modulus 
+
+        cout<<12  %  4 <<endl;
+    return  0;
+}
